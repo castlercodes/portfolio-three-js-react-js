c90242a7b6f052a93105e5152c1a28b34a8b2d26
jeevanalexen <86559072+castlercodes@users.noreply.github.com>
2023-11-26 15:26:05 +0530

    added sound track

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,65 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
 import { Space } from "../models/Space.jsx";
 import './page.css'
+import { Stylized_Planet } from "../models/Stylized_Planet.jsx";
+import { Raycaster } from "three";
+import spacesound1 from "../assets/music/a-space-journey-through-the-solar-system-153272.mp3"
 
 const Home = () => {
+  const [canvasSize, setCanvasSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+  const raycaster = new Raycaster();
+  const [isPlayingspacesound1, setIsPlayingspacesound1] = useState(true);
+
+  const spacesound1Ref = useRef(new Audio(spacesound1));
+  spacesound1Ref.current.loop = true;
+
+  useEffect(() => {
+    if(isPlayingspacesound1){
+      spacesound1Ref.current.play();
+    }
+  }, [isPlayingspacesound1])
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCanvasSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const adjustStylizedPlanetForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (canvasSize.width < 1200) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [canvasSize.width / 300, 5, -8];
+    } else {
+      screenScale = [0.8, 1, 0.8];
+      screenPosition = [5, 2, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const handleClickStylizedPlanet = () => {
+
+  }
+
+  const [biStylizedPlanetScale, biStylizedPlanetPosition] = adjustStylizedPlanetForScreenSize();
+
   return (
     <section className="home">
-      <Canvas camera={{ near: 0.1, far: 1000 }} style={{ height: "100vh", width: "100%" }}>
+      <Canvas camera={{ near: 0.1, far: 1000 }} style={{ height: canvasSize.height, width: canvasSize.width }}>
         <directionalLight position={[1, 1, 1]} intensity={2} />
         <ambientLight intensity={0.5} />
 
         <Space position={[0, -1, 0]} />
+        <group onClick={handleClickStylizedPlanet}><Stylized_Planet position={biStylizedPlanetPosition} scale={biStylizedPlanetScale} /></group>
       </Canvas>
     </section>
   );
